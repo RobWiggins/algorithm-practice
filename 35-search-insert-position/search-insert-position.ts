ecad04cac3822fa51c9c6ea45679f84313710d09
Rob Wiggins <heartofshade@gmail.com>
2026-05-26 17:10:30 -0400

    Time: 0 ms (100.00%) | Memory: 56.2 MB (10.05%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.ts
@@ -0,0 +1,20 @@
+function searchInsert(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+      const middle = Math.floor((left + right) / 2);
+
+      if (nums[middle] === target) {
+        return middle;
+      }
+
+      if (target < nums[middle]) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+    }
+
+    return left;
+};
