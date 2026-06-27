4cac64b12675a286ab495aee18c5903a3e9e5a37
Rob Wiggins <heartofshade@gmail.com>
2026-06-27 14:52:35 -0400

    Time: 2 ms (53.27%) | Memory: 58.7 MB (28.15%) - LeetSync

--- 268-missing-number/missing-number.ts
+++ 268-missing-number/missing-number.ts
@@ -1,12 +1,9 @@
 function missingNumber(nums: number[]): number {
     const seen = Array(nums.length + 1).fill(-1)
 
-    // forget last element??
     for (let i = 0; i < nums.length; i++) {
         seen[nums[i]] = nums[i]
     }
-
-    console.log('seen', seen)
 
     for (let i = 0; i < seen.length; i++) {
         if (seen[i] === -1) {
