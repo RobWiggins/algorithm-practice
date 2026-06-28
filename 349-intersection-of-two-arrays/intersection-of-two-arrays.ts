47c4b392c05178d7dde5ff961444ba8301e1736d
Rob Wiggins <heartofshade@gmail.com>
2026-06-28 13:08:36 -0400

    Time: 1 ms (79.88%) | Memory: 57 MB (55.53%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.ts
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.ts
@@ -1,29 +1,3 @@
-// function intersection(nums1: number[], nums2: number[]): number[] {
-    
-//     const nums1Sorted = nums1.toSorted((a, b) => a - b)
-//     const nums2Sorted = nums2.toSorted((a, b) => a - b)
-
-//     let ptr1 = 0
-//     let ptr2 = 0
-//     const intersectionSet: Set<number> = new Set()
-
-//     while (ptr1 < nums1Sorted.length && ptr2 < nums2Sorted.length) {
-//         if (nums1Sorted[ptr1] === nums2Sorted[ptr2]) {
-//             intersectionSet.add(nums1Sorted[ptr1])
-//             ptr1++
-//             ptr2++
-//         } else if (nums1Sorted[ptr1] < nums2Sorted[ptr2]) {
-//             ptr1++
-//         } else {
-//             ptr2++
-//         }
-//     }
-    
-//     return Array.from(intersectionSet)
-// };
-
-// OR MORE EFFICIENTLY
-
 function intersection(nums1: number[], nums2: number[]): number[] {
   const nums1Set = new Set(nums1)
   const result = new Set<number>()
@@ -36,3 +10,33 @@ function intersection(nums1: number[], nums2: number[]): number[] {
 
   return Array.from(result)
 }
+
+
+/**
+* Less efficient two pointer technique.
+* 
+*
+function intersection(nums1: number[], nums2: number[]): number[] {
+    
+    const nums1Sorted = nums1.toSorted((a, b) => a - b)
+    const nums2Sorted = nums2.toSorted((a, b) => a - b)
+
+    let ptr1 = 0
+    let ptr2 = 0
+    const intersectionSet: Set<number> = new Set()
+
+    while (ptr1 < nums1Sorted.length && ptr2 < nums2Sorted.length) {
+        if (nums1Sorted[ptr1] === nums2Sorted[ptr2]) {
+            intersectionSet.add(nums1Sorted[ptr1])
+            ptr1++
+            ptr2++
+        } else if (nums1Sorted[ptr1] < nums2Sorted[ptr2]) {
+            ptr1++
+        } else {
+            ptr2++
+        }
+    }
+    
+    return Array.from(intersectionSet)
+};
+*/
