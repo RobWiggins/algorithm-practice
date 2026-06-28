2038651f5831ce4b0dd3a3903ee2edbc110f0d81
Rob Wiggins <heartofshade@gmail.com>
2026-06-28 13:02:59 -0400

    Time: 1 ms (79.88%) | Memory: 57.4 MB (41.85%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.ts
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.ts
@@ -1,23 +1,38 @@
+// function intersection(nums1: number[], nums2: number[]): number[] {
+    
+//     const nums1Sorted = nums1.toSorted((a, b) => a - b)
+//     const nums2Sorted = nums2.toSorted((a, b) => a - b)
+
+//     let ptr1 = 0
+//     let ptr2 = 0
+//     const intersectionSet: Set<number> = new Set()
+
+//     while (ptr1 < nums1Sorted.length && ptr2 < nums2Sorted.length) {
+//         if (nums1Sorted[ptr1] === nums2Sorted[ptr2]) {
+//             intersectionSet.add(nums1Sorted[ptr1])
+//             ptr1++
+//             ptr2++
+//         } else if (nums1Sorted[ptr1] < nums2Sorted[ptr2]) {
+//             ptr1++
+//         } else {
+//             ptr2++
+//         }
+//     }
+    
+//     return Array.from(intersectionSet)
+// };
+
+// OR MORE EFFICIENTLY
+
 function intersection(nums1: number[], nums2: number[]): number[] {
-    
-    const nums1Sorted = nums1.toSorted((a, b) => a - b)
-    const nums2Sorted = nums2.toSorted((a, b) => a - b)
+  const nums1Set = new Set(nums1)
+  const result = new Set<number>()
 
-    let ptr1 = 0
-    let ptr2 = 0
-    const intersectionSet: Set<number> = new Set()
-
-    while (ptr1 < nums1Sorted.length && ptr2 < nums2Sorted.length) {
-        if (nums1Sorted[ptr1] === nums2Sorted[ptr2]) {
-            intersectionSet.add(nums1Sorted[ptr1])
-            ptr1++
-            ptr2++
-        } else if (nums1Sorted[ptr1] < nums2Sorted[ptr2]) {
-            ptr1++
-        } else {
-            ptr2++
-        }
+  for (const num of nums2) {
+    if (nums1Set.has(num)) {
+        result.add(num)
     }
-    
-    return Array.from(intersectionSet)
-};
+  }
+
+  return Array.from(result)
+}
