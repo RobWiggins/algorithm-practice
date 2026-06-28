07aaf9181a1ba257e27e5f5d0879a1d9ad3c8240
Rob Wiggins <heartofshade@gmail.com>
2026-06-28 12:55:43 -0400

    Time: 10 ms (5.84%) | Memory: 57 MB (55.53%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.ts
@@ -0,0 +1,26 @@
+function intersection(nums1: number[], nums2: number[]): number[] {
+    
+    const nums1Sorted = nums1.toSorted((a, b) => a - b)
+    const nums2Sorted = nums2.toSorted((a, b) => a - b)
+
+    let ptr1 = 0
+    let ptr2 = 0
+    const intersectionSet: Set<number> = new Set()
+
+    while (ptr1 < nums1Sorted.length && ptr2 < nums2Sorted.length) {
+        if (nums1Sorted[ptr1] === nums2Sorted[ptr2]) {
+            intersectionSet.add(nums1Sorted[ptr1])
+            ptr1++
+            ptr2++
+        } else if (nums1Sorted[ptr1] < nums2Sorted[ptr2]) {
+            ptr1++
+        } else {
+            ptr2++
+        }
+    }
+    
+    console.log('intersectionSet', intersectionSet)
+    
+    return Array.from(intersectionSet)
+
+};
