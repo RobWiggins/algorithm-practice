46f324cb7a6154d7fb6de5d9cc2469ae698322d2
Rob Wiggins <heartofshade@gmail.com>
2026-07-01 17:04:10 -0400

    Time: 3 ms (61.75%) | Memory: 59 MB (9.42%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -2,17 +2,16 @@ function twoSum(nums: number[], target: number): number[] {
     const numsMap = new Map<number, number>()
 
     for (let i = 0; i < nums.length; i++) {
-        numsMap.set(i, nums[i])
+        numsMap.set(nums[i], i)
     }
 
     for (let i = 0; i < nums.length; i++) {
-        let needed = target - numsMap.get(i)
-        for (let j = 0; j < nums.length; j++) {
-            if ((i !== j) && numsMap.get(i) + numsMap.get(j) === target) {
-                return [i, j]
+        let needed = target - nums[i]
+        
+            if (numsMap.has(needed) && numsMap.get(needed) !== i) {
+                return [i, numsMap.get(needed)]
             }
         }
-    }
 
-    return [-1, -1]
+    return []
 };
