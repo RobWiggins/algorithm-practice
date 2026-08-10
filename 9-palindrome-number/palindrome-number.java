5e7af294cced572f8fba72aade9a22b15e4b211a
Rob Wiggins <wigginsro11@gmail.com>
2026-08-10 10:41:15 -0400

    Time: 8 ms (7.12%) | Memory: 45.9 MB (72.49%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,12 +1,7 @@
-// import java.lang.StringBuilder;
-
 class Solution {
     public boolean isPalindrome(int x) {
         String xStr = "" + x;
-
         String reversed = new StringBuilder(xStr).reverse().toString();
-
         return xStr.equals(reversed);
-
     }
 }
